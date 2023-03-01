48b5447f7ce6afdd40ff085b2cc1ab6b8107a108
Nargiza Osmonalieva <nargizaosmon@gmail.com>
2023-03-01 07:35:10 +0000

    add spans and attributes

--- backend-flask/app.py
+++ backend-flask/app.py
@@ -28,16 +28,17 @@ from opentelemetry.sdk.trace.export import SimpleSpanProcessor, ConsoleSpanExpor
 provider = TracerProvider()
 processor = BatchSpanProcessor(OTLPSpanExporter())
 provider.add_span_processor(processor)
-#Show this in the logs within backend-flask
+#Show this in the logs within backend-flask app STDOUT
 simple_processor = SimpleSpanProcessor(ConsoleSpanExporter())
 provider.add_span_processor(simple_processor)
 
 trace.set_tracer_provider(provider)
 tracer = trace.get_tracer(__name__)
 
+app = Flask(__name__)
+
 # HoneyComb -----
 # Initialize automatic instrumentation with Flask
-app = Flask(__name__)
 FlaskInstrumentor().instrument_app(app)
 RequestsInstrumentor().instrument()
 
