aac5bea92656c19d0d6285b0c4864a2d7ae0ecf1
Nargiza Osmonalieva <nargizaosmon@gmail.com>
2023-03-17 23:14:43 +0000

    adding db-setup and db-sessions

--- backend-flask/db/schema.sql
+++ backend-flask/db/schema.sql
@@ -6,7 +6,6 @@ DROP TABLE IF EXISTS public.activities;
 
 CREATE TABLE public.users (
   uuid UUID DEFAULT uuid_generate_v4() PRIMARY KEY,
-  user_uuid UUID NOT NULL,
   display_name text,
   handle text,
   cognito_user_id text,
@@ -15,6 +14,7 @@ CREATE TABLE public.users (
 
 CREATE TABLE public.activities (
   uuid UUID DEFAULT uuid_generate_v4() PRIMARY KEY,
+  user_uuid UUID NOT NULL,
   message text NOT NULL,
   replies_count integer DEFAULT 0,
   reposts_count integer DEFAULT 0,
