6c7ba55da056f50a4595a02d0a54192af2bc72a0
Nargiza Osmonalieva <nargizaosmon@gmail.com>
2023-03-22 05:44:44 +0000

    implement create activity part 1

--- backend-flask/lib/db.py
+++ backend-flask/lib/db.py
@@ -9,7 +9,20 @@ class Db:
     connection_url = os.getenv("CONNECTION_URL")
     self.pool = ConnectionPool(connection_url)
   # we want to commit data such as an insert
-  def query_commit(self):
+  def query_commit_returning_id(self, sql, *kwargs):
+    print("SQL STATEMENT-[commit with returning]------")
+    try:
+      conn = self.pool.connection()
+      cur =  conn.cursor()
+      cur.execute(sql,kwargs)
+      returning_id = cur.fetchone()[0]
+      conn.commit()
+      return returning_id 
+    except Exception as err:
+      self.print_sql_err(err)
+      #conn.rollback()
+  def query_commit(self, sql):
+    print("SQL STATEMENT-[commit]------")
     try:
       conn = self.pool.connection()
       cur =  conn.cursor()
--- backend-flask/services/create_activity.py
+++ backend-flask/services/create_activity.py
@@ -1,14 +1,14 @@
 import uuid
 from datetime import datetime, timedelta, timezone
 
-#from lib.db import db
+from lib.db import db
+
 class CreateActivity:
   def run(message, user_handle, ttl):
     model = {
       'errors': None,
       'data': None
     }
-    user_uuid = ''
     
     now = datetime.now(timezone.utc).astimezone()
 
@@ -43,7 +43,8 @@ class CreateActivity:
         'message': message
       }   
     else:
-      self.create_activity()
+      expires_at = (now + ttl_offset)
+      CreateActivity.create_activity(user_handle,message,expires_at)
       model['data'] = {
         'uuid': uuid.uuid4(),
         'display_name': 'Andrew Brown',
@@ -53,7 +54,7 @@ class CreateActivity:
         'expires_at': (now + ttl_offset).isoformat()
       }
     return model
-  def create_activity(user_uuid, message, expires_at):
+  def create_activity(handle, message, expires_at):
     sql = f"""
     INSERT INTO (
       user_uuid,
@@ -61,9 +62,18 @@ class CreateActivity:
       expires_at
     )
     VALUES (
-      "{user_uuid}",
-      "{message}",
-      "{expires_at}"
-    )
+      (SELECT uuid 
+       FROM public.users 
+       WHERE users.handle = %(handle)s
+       LIMIT 1
+      ),
+      %(message)s,
+      %(expires_at)s,
+    ) RETURNING uuid;
     """
-    #query_commit(sql)
+  uuid = db.query_commit_returning_id(sql,
+    handle=handle,
+    message=message,
+    expires_at=expires_at
+  )
+  #def query_object_activity():
